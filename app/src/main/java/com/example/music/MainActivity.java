3cf3d57e520faa7e8be6dcf8e1c0cc8bac4e5407
maksud9998 <bmaksud41@gmail.com>
2021-06-06 16:40:04 +0530

    Splash Screen Addded

--- app/src/main/java/com/example/music/MainActivity.java
+++ app/src/main/java/com/example/music/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.music;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static int timeOut=3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+//                Intent i = new Intent(MainActivity.this,HomePageActivity.class);
+//                startActivity(i);
+//                finish();
+                Toast.makeText(MainActivity.this, "Testing",Toast.LENGTH_SHORT).show();
+            }
+        }, timeOut);
     }
 }
