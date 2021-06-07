bbf5f8ddf86931bd2bdfcf80236c542fb3ecfccb
maksud9998 <bmaksud41@gmail.com>
2021-06-07 11:40:11 +0530

    All icons are converted to white

--- app/src/main/java/com/example/music/SongsPageActivity.java
+++ app/src/main/java/com/example/music/SongsPageActivity.java
@@ -3,19 +3,12 @@ package com.example.music;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-
 import com.example.music.ui.main.SectionsPagerAdapter;
 import com.example.music.databinding.ActivitySongsPageBinding;
 
@@ -35,7 +28,8 @@ BottomNavigationView bottomNav;
         tabs.setupWithViewPager(viewPager);
         bottomNav=findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navlistener);
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();    }
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
+    }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navlistener = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
